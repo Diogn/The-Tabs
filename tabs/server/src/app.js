e5ff30ec961a73b4be1cfcd76a9676c03e8a7a5a
Diogn <rino.dwicahyo@gmail.com>
2018-06-30 23:21:12 +0700

    setting app.js in server folder with localhost trial in port 8081/status

--- tabs/server/src/app.js
+++ tabs/server/src/app.js
@@ -1,1 +1,17 @@
-console.log('hello world!')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'Yo S E M A N G A A T!!'
+  })
+})
+
+app.listen(process.env.PORT || 8081)
