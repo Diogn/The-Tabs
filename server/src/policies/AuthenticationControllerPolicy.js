bfc7161d97bb511f840179e09dedc59a71afc62a
Diogn <rino.dwicahyo@gmail.com>
2018-07-05 14:58:12 +0700

    set error when validate registry

--- server/src/policies/AuthenticationControllerPolicy.js
+++ server/src/policies/AuthenticationControllerPolicy.js
@@ -8,6 +8,32 @@ module.exports = {
         new RegExp('^[a-zA-Z0-9]{8,32}$')
       )
     }
-    next()
+    const {error} = Joi.validate(req.body, schema)
+
+    if (error) {
+      switch (error.details[0].context.key) {
+        case 'email':
+          res.status(404).send({
+            error: 'You must provide a valid email address'
+          })
+          break
+        case 'password':
+          res.status(404).send({
+            error: `The Password provided failed to match the following rules:
+            <br>
+            1. It must contain ONLY the following characters: lower case, upper case, numerics.
+            <br>
+            2. It must be at least 8 characters in length and not greater than 32 character.
+            `
+          })
+          break
+        default:
+          res.status(404).send({
+            error: 'Invalid registration information'
+          })
+      }
+    } else {
+      next()
+    }
   }
 }
