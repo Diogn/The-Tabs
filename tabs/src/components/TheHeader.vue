283afa996e5bb0fcc02994523ace7696d40cd1de
Diogn <rino.dwicahyo@gmail.com>
2018-07-07 10:14:51 +0700

    adding function into toolbar

--- tabs/src/components/TheHeader.vue
+++ tabs/src/components/TheHeader.vue
@@ -1,17 +1,46 @@
 <template>
   <v-toolbar fixed dark class="cyan">
     <v-toolbar-title class="cyan">
-      Register
+      <span class="home-btn"
+      @click="navigateTo({name: 'home'})">
+        THE TABS
+      </span>
     </v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn flat dark
+      @click="navigateTo({name: 'register'})">
+        Sign Up
+      </v-btn>
+      <v-btn flat dark
+      v-on:click="goBack">
+        GoBack!
+      </v-btn>
+    </v-toolbar-items>
   </v-toolbar>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    },
+    goBack () {
+      window.history.length > 1
+      ? this.$router.go(-1)
+      : this.$router.push('/')
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.home-btn {
+  cursor: pointer;
+}
+.home-btn:hover {
+  color: grey;
+}
 </style>
