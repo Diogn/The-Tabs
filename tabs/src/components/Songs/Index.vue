288fd2934b2079af5472f2bf1ee0c57b255e0b5a
Rino_DC <rino.dwicahyo@gmail.com>
2018-07-29 23:33:48 +0700

    sets history and passport authentication

--- tabs/src/components/Songs/Index.vue
+++ tabs/src/components/Songs/Index.vue
@@ -1,10 +1,15 @@
 <template>
   <v-layout>
-    <v-flex xs6 class="mr-2">
+    <v-flex xs6 class="mr-2"
+      v-if="isUserLoggedIn">
       <songs-bookmarks />
+      <recently-viewed-songs class="mt-2" />
     </v-flex>
 
-    <v-flex xs6>
+    <v-flex :class="{
+      xs12: !isUserLoggedIn,
+      xs6: isUserLoggedIn
+      }">
       <songs-search-panel class="mb-2"/>
       <songs-panel />
     </v-flex>
@@ -12,13 +17,24 @@
 </template>
 
 <script>
+import RecentlyViewedSongs from './RecentlyViewedSongs'
 import SongsBookmarks from './SongsBookmarks'
 import SongsPanel from './SongsPanel'
 import SongsSearchPanel from './SongsSearchPanel'
 import SongsService from '@/services/SongsService'
+import {mapState} from 'vuex'
+
 export default {
   components: {
-    SongsPanel, SongsSearchPanel, SongsBookmarks
+    SongsPanel,
+    SongsSearchPanel,
+    SongsBookmarks,
+    RecentlyViewedSongs
+  },
+  computed: {
+    ...mapState([
+      'isUserLoggedIn'
+    ])
   },
   data () {
     return {
--- tabs/src/components/ViewSong/SongMetadata.vue
+++ tabs/src/components/ViewSong/SongMetadata.vue
@@ -62,7 +62,8 @@ export default {
   },
   computed: {
     ...mapState([
-      'isUserLoggedIn'
+      'isUserLoggedIn',
+      'user'
     ])
   },
   watch: {
@@ -72,11 +73,12 @@ export default {
       }
 
       try {
-        const query = {
-          songId: this.song.id,
-          userId: this.$store.state.user.id
+        const bookmarks = (await BookmarksService.index({
+          songId: this.song.id
+        })).data
+        if (bookmarks.length) {
+          this.bookmark = bookmarks[0]
         }
-        this.bookmark = (await BookmarksService.index(query)).data
       } catch (err) {
         console.log(err)
       }
@@ -85,11 +87,9 @@ export default {
   methods: {
     async setAsBookmark () {
       try {
-        const bookmark = {
-          songId: this.song.id,
-          userId: this.$store.state.user.id
-        }
-        this.bookmark = (await BookmarksService.post(bookmark)).data
+        this.bookmark = (await BookmarksService.post({
+          songId: this.song.id
+        })).data
       } catch (err) {
         console.log(err)
       }
